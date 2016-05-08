7bfcb9b5768861059807d75edca9c8d8c29a201f
Bernhard Loibl <bernhard.loibl@gmail.com>
2016-05-08 13:30:29 +0200

    Initial Commit

---
+++ UIKonf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKonf
+//
+//  Created by Bernhard Loibl on 08/05/16.
+//  Copyright © 2016 Raised by Wolves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
