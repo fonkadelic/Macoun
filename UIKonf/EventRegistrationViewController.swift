634d29bc4278969f95f41955afec260edae1e9ca
Bernhard Loibl <bernhard.loibl@gmail.com>
2016-05-09 08:25:08 +0200

    Adds view controller for event registration.

---
+++ UIKonf/EventRegistrationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventRegistrationViewController.swift
+//  UIKonf
+//
+//  Copyright © 2016 Raised by Wolves. All rights reserved.
+//
+
+import UIKit
+
+class EventRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
