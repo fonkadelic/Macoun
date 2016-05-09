ae901402cbcfe9846defa451aa72ffed479ddb09
Bernhard Loibl <bernhard.loibl@gmail.com>
2016-05-09 07:59:58 +0200

    Presents events.

--- UIKonf/Event.swift
+++ UIKonf/Event.swift
@@ -21,37 +21,37 @@ extension Event {
             id: 0,
             name: "Bike Tour",
             description: "UIKonf bike tour has become a tradition. The tour of 2016 will continue that tradition.",
-            imageName: "bike-tour.jpg"
+            imageName: "bike-tour"
         ),
         Event(
             id: 1,
             name: "A Boat Tour on the Spree",
             description: "Relax, enjoy Berlin from a boat and chat with other UIKonf participants.",
-            imageName: "boat-tour.jpg"
+            imageName: "boat-tour"
         ),
         Event(
             id: 2,
             name: "Brewery Tour",
             description: "Follow a German tradition and learn how to brew beer at the Brewery Tour.",
-            imageName: "brewer-tour.jpg"
+            imageName: "brewery-tour"
         ),
         Event(
             id: 3,
             name: "WWII Bomb-Shelter Tour",
             description: "A guided tour of a WWII bomb shelter",
-            imageName: "shelter-tour.jpg"
+            imageName: "shelter-tour"
         ),
         Event(
             id: 4,
             name: "Stasi Museum Tour",
             description: "Visit the former GDR state security head office which has been conserved in time.",
-            imageName: "stasi-tour.jpg"
+            imageName: "stasi-tour"
         ),
         Event(
             id: 5,
             name: "Tempelhof Airport Tour",
             description: "Ever wondered how Tempelhof airport looks from the inside? Then join this tour.",
-            imageName: "airport-tour.jpg"
+            imageName: "airport-tour"
         ),
     ]
 }
--- UIKonf/EventListViewController.swift
+++ UIKonf/EventListViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class EventListViewController: UITableViewController {
+    
+    let events = Event.allEvents
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.registerNib(UINib(nibName: String(BasicTableViewCell), bundle: nil), forCellReuseIdentifier:String(BasicTableViewCell))
+        tableView.rowHeight = 100
     }
     
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(String(BasicTableViewCell), forIndexPath: indexPath) as! BasicTableViewCell
+        let event = events[indexPath.row]
+        cell.configure(withTitle: event.name, detail: event.description, imageName: event.imageName)
+        
+        return cell
+    }
 }
