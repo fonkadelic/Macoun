bf4e4df9b3a02cd5e8d4230f1f3d64f883cc9f4a
Mathias Nagler <mathias.nagler@me.com>
2016-05-15 17:56:01 +0200

    Adds registration complete view

--- UIKonf/EventRegistrationViewController.swift
+++ UIKonf/EventRegistrationViewController.swift
@@ -11,10 +11,22 @@ class EventRegistrationViewController: UIViewController {
     
     var event: Event!
     @IBOutlet var headerView: HeaderView!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var registrationCompletedView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         headerView.configure(withTitle: event.name, imageResource: event.imageResource)
+    }
+    
+    @IBAction func registerButtonTapped(sender: UIButton) {
+        scrollView.hidden = true
+        
+        registrationCompletedView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(registrationCompletedView)
+        registrationCompletedView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor).active = true
+        registrationCompletedView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor).active = true
+        registrationCompletedView.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
     }
 }
