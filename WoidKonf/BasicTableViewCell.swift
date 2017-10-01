9452d32a1e268ace92310a262bd7ff702585ff04
Bernhard Loibl <mail@bernhardloibl.com>
2017-10-01 13:51:49 +0200

    Applies styles.

--- WoidKonf/BasicTableViewCell.swift
+++ WoidKonf/BasicTableViewCell.swift
@@ -14,6 +14,14 @@ class BasicTableViewCell: AnimatableTableViewCell, NibReusable {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     @IBOutlet var pictureView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        let selected = UIView()
+        selected.backgroundColor = UIColor(named: "selected")?.withAlphaComponent(0.8)
+        selectedBackgroundView = selected
+    }
     
     func configure(withTitle title: String, detail: String, image: UIImage) {
         titleLabel.text = title
