ede87a6d3caefdb32d72c107c898f98eb344c5f6
Mathias Nagler <mathias.nagler@me.com>
2017-10-05 14:46:58 +0200

    Use non-generic ViewControllers for demo

--- WoidKonf/EventListViewController.swift
+++ WoidKonf/EventListViewController.swift
@@ -8,25 +8,105 @@
 import UIKit
 import Reusable
 
-final class EventListViewController: TableViewController<Event, BasicTableViewCell> {
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-
-        items = Event.allEvents
-        configureCell = { cell, event in
-            cell.accessoryType = .disclosureIndicator
-            cell.configure(withTitle: event.name, detail: event.description, image: event.image)
-        }
-        willDisplayCell = { cell in
-            
-        }
-        didSelect = { [unowned self] event in
-            self.perform(segue: StoryboardSegue.Main.showEventRegistration, prepare: { segue in
-                guard let detail = segue.destination as? EventRegistrationViewController
-                    else { fatalError("Wrong view controller type.") }
-                detail.event = event
-            })
-        }
+final class EventListViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // register nib
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.perform(segue: StoryboardSegue.Main.showEventRegistration, prepare: { segue in
+            guard let detail = segue.destination as? EventRegistrationViewController else { fatalError("Wrong view controller type.") }
+            let event = Event.allEvents[indexPath.row]
+            detail.event = event
+        })
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = Event.allEvents[indexPath.row]
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: BasicTableViewCell.self)
+        cell.accessoryType = .disclosureIndicator
+        cell.configure(withTitle: item.name, detail: item.description, image: item.image)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Event.allEvents.count
+    }
+    
+    // MARK: - Segues
+    
+    private var prepareHandlers: [String: (UIStoryboardSegue) -> ()] = [:]
+    
+    final func perform<S: SegueType>(segue: S, prepare: @escaping (UIStoryboardSegue) -> ()) where S.RawValue == String {
+        prepareHandlers[segue.rawValue] = prepare
+        performSegue(withIdentifier: segue.rawValue, sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier,
+            let handler = prepareHandlers.removeValue(forKey: identifier) else { fatalError() }
+        
+        handler(segue)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//final class EventListViewController: TableViewController<Event, BasicTableViewCell> {
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//
+//        items = Event.allEvents
+//        configureCell = { cell, event in
+//            cell.accessoryType = .disclosureIndicator
+//            cell.configure(withTitle: event.name, detail: event.description, image: event.image)
+//        }
+//        willDisplayCell = { cell in
+//            
+//        }
+//        didSelect = { [unowned self] event in
+//            self.perform(segue: StoryboardSegue.Main.showEventRegistration, prepare: { segue in
+//                guard let detail = segue.destination as? EventRegistrationViewController
+//                    else { fatalError("Wrong view controller type.") }
+//                detail.event = event
+//            })
+//        }
+//    }
+//}
+
--- WoidKonf/SpeakerListViewController.swift
+++ WoidKonf/SpeakerListViewController.swift
@@ -8,24 +8,104 @@
 import UIKit
 import Reusable
 
-final class SpeakerListViewController: TableViewController<Speaker, BasicTableViewCell> {
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+final class SpeakerListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // register nib
+    }
 
-        items = Speaker.allSpeakers
-        configureCell = { cell, speaker in
-            cell.accessoryType = .disclosureIndicator
-            cell.configure(withTitle: speaker.name, detail: speaker.shortBio, image: speaker.image)
-        }
-        willDisplayCell = { cell in
-
-        }
-        didSelect = { [unowned self] speaker in
-            self.perform(segue: StoryboardSegue.Main.showSpeakerDetail, prepare: { segue in
-                guard let detail = segue.destination as? SpeakerDetailViewController
-                    else { fatalError("Wrong view controller type.") }
-                detail.speaker = speaker
-            })
-        }
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.perform(segue: StoryboardSegue.Main.showSpeakerDetail, prepare: { segue in
+            guard let detail = segue.destination as? SpeakerDetailViewController else { fatalError("Wrong view controller type.") }
+            let speaker = Speaker.allSpeakers[indexPath.row]
+            detail.speaker = speaker
+        })
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = Speaker.allSpeakers[indexPath.row]
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: BasicTableViewCell.self)
+        cell.accessoryType = .disclosureIndicator
+        cell.configure(withTitle: item.name, detail: item.shortBio, image: item.image)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Speaker.allSpeakers.count
+    }
+    
+    // MARK: - Segues
+    
+    private var prepareHandlers: [String: (UIStoryboardSegue) -> ()] = [:]
+    
+    final func perform<S: SegueType>(segue: S, prepare: @escaping (UIStoryboardSegue) -> ()) where S.RawValue == String {
+        prepareHandlers[segue.rawValue] = prepare
+        performSegue(withIdentifier: segue.rawValue, sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier,
+            let handler = prepareHandlers.removeValue(forKey: identifier) else { fatalError() }
+        
+        handler(segue)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//final class SpeakerListViewController: TableViewController<Speaker, BasicTableViewCell> {
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//
+//        items = Speaker.allSpeakers
+//        configureCell = { cell, speaker in
+//            cell.accessoryType = .disclosureIndicator
+//            cell.configure(withTitle: speaker.name, detail: speaker.shortBio, image: speaker.image)
+//        }
+//        willDisplayCell = { cell in
+//
+//        }
+//        didSelect = { [unowned self] speaker in
+//            self.perform(segue: StoryboardSegue.Main.showSpeakerDetail, prepare: { segue in
+//                guard let detail = segue.destination as? SpeakerDetailViewController
+//                    else { fatalError("Wrong view controller type.") }
+//                detail.speaker = speaker
+//            })
+//        }
+//    }
+//}
+
